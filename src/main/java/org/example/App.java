0a829b9fe2a3087b38d5a3c04925e48d2f3a1fca
m.fedotov <m.fedotov@gaskar.group>
2024-06-20 11:40:04 +0300

    For Rebase

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        System.out.println("afawfafw");
         System.out.println( "Hell121212o World1111gi" );
         System.out.println(" 1212Hey 21e12e1e1 Hey");
         System.out.println("aawfawf");
